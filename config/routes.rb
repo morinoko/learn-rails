beade31a97385f6c7640d4ac36db17a35d5193e0
Felice Forby <felice@cookpad.com>
2016-03-30 17:16:32 +0900

    add contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :contacts, only: [:new, :create]
+  root to: 'visitors#new'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root to: 'visitors#new'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
